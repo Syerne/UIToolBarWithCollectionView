9e4c4cccc7d1eaa9d5cf61759eb0a5a78852688a
shubham yerne <shubham.vijay@myn.global>
2023-09-28 17:29:46 +0530

    first commit

---
+++ AttributedText/CustomCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCollectionViewCell.swift
+//  AttributedText
+//
+//  Created by shubam on 26/09/23.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ AttributedText/TextStyleCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TextStyleCollectionViewCell.swift
+//  AttributedText
+//
+//  Created by shubam on 27/09/23.
+//
+
+import UIKit
+
+class TextStyleCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
